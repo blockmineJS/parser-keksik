be0673c0018688cae5d514bc9f67af805b1c2e59
mmeerrkkaa <55891034+mmeerrkkaa@users.noreply.github.com>
2025-07-01 17:23:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 {
   "name": "parser-keksik",
-  "version": "1.1.0",
+  "version": "1.1.1",
   "description": "Парсит сообщения чата для mineblaze, dexland, masedworld, cheatmine и генерирует события для команд под их сервера.",
   "main": "index.js",
   "botpanel": {
@@ -10,31 +10,31 @@
       "mc.cheatmine.net",
       "mc.dexland.org"
     ],
-  "settings": {
-    "localDelay": {
-      "type": "number",
-      "label": "Задержка локального чата (мс)",
-      "description": "Задержка после отправки сообщения в локальный чат.",
-      "default": 3000
-    },
-    "globalDelay": {
-      "type": "number",
-      "label": "Задержка глобального чата (мс)",
-      "description": "Задержка после отправки сообщения в глобальный чат.",
-      "default": 1000
-    },
-    "clanDelay": {
-      "type": "number",
-      "label": "Задержка кланового чата (мс)",
-      "description": "Задержка после отправки сообщения в клановый чат.",
-      "default": 500
-    },
-    "privateDelay": {
-      "type": "number",
-      "label": "Задержка приватного чата (мс)",
-      "description": "Задержка после отправки сообщения в приватный чат чат.",
-      "default": 3000
+    "settings": {
+      "localDelay": {
+        "type": "number",
+        "label": "Задержка локального чата (мс)",
+        "description": "Задержка после отправки сообщения в локальный чат.",
+        "default": 3000
+      },
+      "globalDelay": {
+        "type": "number",
+        "label": "Задержка глобального чата (мс)",
+        "description": "Задержка после отправки сообщения в глобальный чат.",
+        "default": 1000
+      },
+      "clanDelay": {
+        "type": "number",
+        "label": "Задержка кланового чата (мс)",
+        "description": "Задержка после отправки сообщения в клановый чат.",
+        "default": 500
+      },
+      "privateDelay": {
+        "type": "number",
+        "label": "Задержка приватного чата (мс)",
+        "description": "Задержка после отправки сообщения в приватный чат чат.",
+        "default": 3000
+      }
     }
   }
-}
 }
